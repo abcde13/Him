c719a3acac33a2290e0ac281eeacff67f7535ec6
Joraaver Chahal <joraaverchahal@gmail.com>
2014-01-24 22:22:58 -0800

    service and message reception up, but not connected

--- src/com/him/MainActivity.java
+++ src/com/him/MainActivity.java
@@ -2,14 +2,20 @@ package com.him;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
+	static TextView messageBox;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+	    messageBox=(TextView)findViewById(R.id.messageBox);
 	}
 
 	@Override
@@ -18,5 +24,15 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
+
+	// Method to start the service
+	public void startService(View view) {
+	   startService(new Intent(getBaseContext(), ReceiverService.class));
+	}
+	
+	public static void updateMessageBox(String msg)
+    {
+    	messageBox.append(msg);
+    }
 
 }
