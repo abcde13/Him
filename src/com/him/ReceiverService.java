86d67421cd19b2d2148cfe3d3d95a59b85525b57
Joraaver Chahal <joraaverchahal@gmail.com>
2014-01-24 23:20:00 -0800

    Trying inital parse

--- src/com/him/ReceiverService.java
+++ src/com/him/ReceiverService.java
@@ -2,6 +2,7 @@ package com.him;
 
 import android.app.Service;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.IBinder;
 import android.widget.Toast;
 
@@ -15,7 +16,17 @@ public class ReceiverService extends Service {
    public int onStartCommand(Intent intent, int flags, int startId) {
       // Let it continue running until it is stopped.
       Toast.makeText(this, "Service Started", Toast.LENGTH_LONG).show();
+      
+      /*TextMessageReceiver mSmsReceiver = new TextMessageReceiver();
+      IntentFilter filter = new IntentFilter();
+      filter.setPriority(IntentFilter.SYSTEM_HIGH_PRIORITY);
+      filter.addAction(SMS_RECEIVE_ACTION); // SMS
+      filter.addAction(WAP_PUSH_RECEIVED_ACTION); // MMS
+      this.registerReceiver(mSmsReceiver, filter);*/
+      
       return START_STICKY;
+      
+      
    }
    @Override
    public void onDestroy() {
